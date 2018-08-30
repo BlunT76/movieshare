78c7331860daeda17f7da1d45929172a6cf49aa7
100drine <julien100drine@sfr.fr>
2018-08-30 10:20:02 +0200

    added cards

--- fuel/app/views/film/index.php
+++ fuel/app/views/film/index.php
@@ -1,42 +1,49 @@
 <h2>Listing <span class='muted'>Films</span></h2>
 <br>
 <?php if ($films): ?>
-<table class="table table-striped">
-	<thead>
-		<tr>
-			<th>Title</th>
-			<th>Year</th>
-			<th>Director</th>
-			<th>Actors</th>
-			<th>Runtime</th>
-			<th>Plot</th>
-			<th>Rented</th>
-			<th>Poster</th>
-			<th>&nbsp;</th>
-		</tr>
-	</thead>
-	<tbody>
-<?php foreach ($films as $item): ?>		<tr>
+<div class="row">
 
-			<td><?php echo $item->title; ?></td>
-			<td><?php echo $item->year; ?></td>
-			<td><?php echo $item->director; ?></td>
-			<td><?php echo $item->actors; ?></td>
-			<td><?php echo $item->runtime; ?></td>
-			<td><?php echo $item->plot; ?></td>
-			<td><?php echo $item->rented; ?></td>
-			<td><?php echo $item->poster; ?></td>
-			<td>
-				<div class="btn-toolbar">
-					<div class="btn-group">
-						<?php echo Html::anchor('film/view/'.$item->id, '<i class="icon-eye-open"></i> View', array('class' => 'btn btn-default btn-sm')); ?>						<?php echo Html::anchor('film/edit/'.$item->id, '<i class="icon-wrench"></i> Edit', array('class' => 'btn btn-default btn-sm')); ?>						<?php echo Html::anchor('film/delete/'.$item->id, '<i class="icon-trash icon-white"></i> Delete', array('class' => 'btn btn-sm btn-danger', 'onclick' => "return confirm('Are you sure?')")); ?>					</div>
-				</div>
 
-			</td>
-		</tr>
-<?php endforeach; ?>	</tbody>
-</table>
+<?php foreach ($films as $item): ?>	
+<div class="col col-md-4">
+<div class="card" >
+  <img class="card-img-top" src="<?php echo $item->poster; ?>" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $item->title; ?></h5>
+		<ul class="list-group list-group-flush">
+			<li class="list-group-item">Year: <?php echo $item->year; ?></li>
+			<li class="list-group-item">Director: <?php echo $item->director; ?></li>
+			<li class="list-group-item">Actors: <?php echo $item->actors; ?></li>
+			<li class="list-group-item">Run Time: <?php echo $item->runtime; ?></li>
+			<li class="list-group-item">Plot: <?php echo $item->plot; ?></li>
+			<li class="list-group-item">Rented: <?php echo $item->rented; ?></li>
+  		</ul>
+		
+		<?php echo Html::anchor('film/view/'.$item->id, '<i class="icon-eye-open"></i> View', array('class' => 'btn btn-default btn-sm')); ?>
 
+
+		<?php if($_SESSION['role']=='admin'){
+		echo Html::anchor('film/edit/'.$item->id, '<i class="icon-wrench"></i> Edit', array('class' => 'btn btn-default btn-sm')); ?>
+		<?php echo Html::anchor('film/delete/'.$item->id, '<i class="icon-trash icon-white"></i> Delete', array('class' => 'btn btn-sm btn-danger', 'onclick' => "return confirm('Are you sure?')")); }?>	
+  </div>
+</div>
+</div>
+
+
+		
+			
+			
+			
+			
+			
+			
+			
+										
+				
+
+<?php endforeach; ?>	
+</div>
+</div>
 <?php else: ?>
 <p>No Films.</p>
 
